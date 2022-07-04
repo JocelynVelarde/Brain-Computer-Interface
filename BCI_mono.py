cba07d5e9ea56146b8c49a9105d38e35e80cb832
Jocelyn Velarde <a01285780@itesm.mx>
2022-07-03 20:06:26 -0500

    added comments to explain code

--- BCI_mono.py
+++ BCI_mono.py
@@ -7,17 +7,21 @@ import time
 ser = serial.Serial('COM3' , 9600)
 time.sleep(2)
 
+#investigar lo de esta variable
 thresh = 0.5
 
 #pip install pylsl
 from pylsl import StreamInlet, resolve_stream
 
+#resolver un stream EEG desde el lab network 
 print("buscando un controlador de EEG...")
 streams = resolve_stream('type' , 'EEG')
 
+#crear un nuevo inlet para resolver el stream
 inlet = StreamInlet(streams[0])
 
 while True: 
+    #obtener un nuevo sample
     sample, Timestamp = inlet.pull_sample()
     state = sample[0]
     print(state)
